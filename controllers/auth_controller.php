2f5e64828b9b6ee57c4c06fc21d6bc1a23e882f3
AlaaHarmoush1 <Alaacoding@outlook.com>
2025-06-25 13:56:15 +0300

    Added extra headers to work with frontend

--- controllers/auth_controller.php
+++ controllers/auth_controller.php
@@ -3,8 +3,11 @@
 require_once __DIR__ . '/../connection/connection.php';
 require_once __DIR__ . '/../models/User.php';
 
-header("Access-Control-Allow-Origin: *"); 
+header("Access-Control-Allow-Origin: http://127.0.0.1:5500"); 
 header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: POST, GET");
+header("Access-Control-Allow-Headers: Content-Type");
+
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $data = json_decode(file_get_contents("php://input"), true); // this is to decode the JSON data that is comming from the frontend  
